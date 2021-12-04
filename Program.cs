a3c0b0691fd9b0b25871b4452cef78ed0e3d33eb
Carter702 <Carter702@gmail.com>
2021-12-04 11:37:51 -0700

    Initialized project, solved day 1

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+var increases = 0;
+const int BufferSize = 128;
+const string Filename = "input_day1.txt";
+var stopWatch = new Stopwatch();
+
+using var fileStream = File.OpenRead(Filename);
+using var streamReader = new StreamReader(fileStream, System.Text.Encoding.UTF8, true, BufferSize);
+
+var prevLine = streamReader.ReadLine();
+var line = "";
+stopWatch.Start();
+
+while( (line = streamReader.ReadLine()) != null ){
+    increases += (int.Parse(prevLine) < int.Parse(line)) ? 1 : 0;
+    prevLine = line;
+}
+
+stopWatch.Stop();
+Console.WriteLine($"Finished in {stopWatch.Elapsed.Milliseconds} ms, Increases: {increases}");
